3bdd556635fd7987ab69e24a40e8d6fcb8b57bf2
KalhariEkanayake <kalharichathumini@gmail.com>
2024-06-17 23:12:50 +0530

    Capture QueryParams Using RequestParam and First Look at Model

--- myfirstwebapplication/src/main/java/com/kalhari/springboot/myfirstwebapplication/login/LoginController.java
+++ myfirstwebapplication/src/main/java/com/kalhari/springboot/myfirstwebapplication/login/LoginController.java
@@ -1,13 +1,20 @@
 package com.kalhari.springboot.myfirstwebapplication.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
+	// /login => com.kalhari.springboot.myfirstwebapplication.login.LoginController => login.jsp
+	// http://localhost:8080/login?name=Kalhari
+	// Model
 	@RequestMapping("login")
-	public String gotoLoginPage() {
+	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
+		System.out.println("Request param is " + name); // NOT RECOMMENDED FOR PRODUCTION CODE
 		return "login";
 	}
 }
--- myfirstwebapplication/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+++ myfirstwebapplication/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -3,6 +3,8 @@
 	<title>My First HTML Page - JSP</title>
 	</head>
 	<body>
+		<h1>Heading 01</h1>
+		<h2>Heading 02</h2>
 		My first HTML page with body - JSP
 	</body>
 </html>
